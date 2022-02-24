6b1706e74935897589949f87bc74e1cfa2bdab50
alemica97 <alessandro.micalizzi08@gmail.com>
2022-02-24 21:40:49 +0100

    bonus 3 - Ho fatto in modo che quando il mouse entra nel container lo slider si blocca e una volta che esce, lo slider riparte

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 let activeClass = 0;
+let slide;
 
 const app = new Vue({
     el : '#app',
     data : {
+        //faccio un array di oggetti, ogni oggetto ha un immagine un titoloo e una descrizione
         carouselObjects : [
             {
                 image : 'img/img-1.jpg',
@@ -35,15 +37,20 @@ const app = new Vue({
                 description : 'L\'Etna è uno stratovulcano complesso della Sicilia originatosi nel Quaternario, ed è il più alto vulcano attivo della placca euroasiatica. Le sue frequenti eruzioni nel corso del tempo hanno modificato, a volte anche profondamente, il paesaggio circostante e in tante occasioni hanno costituito una minaccia per gli insediamenti abitativi nati nel tempo alle sue pendici. Il 21 giugno 2013, la XXXVII sessione del Comitato UNESCO ha inserito l\'Etna nell\'elenco dei beni costituenti il Patrimonio dell\'umanità.'    
             },
         ],
+        //chiavi dell'oggetto data che passo al DOM tramite VUE
+        vueSlider : 'main-title',
+        vueSliderContent : 'VUE - Slider',
         carouselClass : 'item',
         carouselTitleClass : 'title',
         carouselDescriptionClass : 'description',
         controlClass : 'control-wrapper'
     },
+    //con mounted faccio eseguire il metodo slideCarousel appena la pagina viene caricata
     mounted: function(){
         this.slideCarousel();
     },
     methods : {
+        //funzione che passa all'immagine successiva quando viene cliccata la freccetta in basso
         nextImg : function(){
             if(activeClass < this.carouselObjects.length -1){
                 removeActive();
@@ -55,6 +62,7 @@ const app = new Vue({
                 addActive();
             }
         },
+        //funzione che passa all'immagine precedente quando viene cliccata la freccetta in alto
         prevImg : function(){
             if(activeClass > 0){
                 removeActive();
@@ -66,13 +74,21 @@ const app = new Vue({
                 addActive();
             }
         },
+        //tramite v-for passo come parametro di questa funzione l'indice della thumb che viene cliccata
+        //così da dargli la classe 'active'
         actualImg : function(index){
             removeActive();
             activeClass = index;
             addActive();
         },
+        //creo un metodo che contiene una timing function che ogni tre secondi invoca la funzione nextImg
         slideCarousel: function () {
-            setInterval(this.nextImg, 3000);
+            console.log('mouse leave!!');
+            slide = setInterval(this.nextImg, 3000);
+        },
+        stopSlideCarousel: function () {
+            console.log('mouse over!!');
+            clearInterval(slide);
         }
     }
 });
